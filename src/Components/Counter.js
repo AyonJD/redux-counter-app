5fcf879613d941139f2d084ccc2ec95415719093
Ayon Jodder <ayonjodder177@gmail.com>
2022-08-04 23:14:21 +0600

    Dispatch with reset function done

--- src/Components/Counter.js
+++ src/Components/Counter.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { decreaseCount, increaseCount } from '../Source/actionCounter';
+import { decreaseCount, increaseCount, resetCount } from '../Source/actionCounter';
 
 const Counter = () => {
     //Get the current count from the store
@@ -20,13 +20,19 @@ const Counter = () => {
         //Dispatch the action to increase the count by importing the decreaseCount action from the actionCounter.js file
         dispatch(decreaseCount());
     }
+
+    //Handle the reset count button click
+    const handleReset = () => {
+        //Dispatch the action to increase the count by importing the resetCount action from the actionCounter.js file
+        dispatch(resetCount());
+    }
     return (
         <div>
             <div>
                 <h1>{count}</h1>
                 <button onClick={handleIncrease}>+</button>
                 <button onClick={handleDecrease}>-</button>
-                <button>Reset</button>
+                <button onClick={handleReset}>Reset</button>
             </div>
         </div>
     );
